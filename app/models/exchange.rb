91b74e9d288212595152d823fcc54d944c3789e0
francisco <franciscoantunesbarreto1997@gmail.com>
2020-05-23 20:40:25 +0100

    added basic validations

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,4 +1,7 @@
 class Exchange < ApplicationRecord
   belongs_to :user
   belongs_to :item
+
+  validates :description, presence: true, length: { minimum: 25 }
+  validates :code, presence: true, uniqueness: true, length: { minimum: 20 }
 end

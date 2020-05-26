ed154ca7ee2d6cc9541cf0a02179754ba7109220
francisco <franciscoantunesbarreto1997@gmail.com>
2020-05-26 14:45:11 +0100

    started features section on landing page and made the categories section better for mobile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Category.create!(name: "Books")
+# Category.create!(name: "Technology")
+# Category.create!(name: "Clothing")
+# Category.create!(name: "Furniture")
+# Category.create!(name: "Tools")
+# Category.create!(name: "Toys")
+# Category.create!(name: "Others")
+
+Item.destroy_all
+
+
+5.times do
+  Item.create!(title: "My item",
+               description: "blablablablablablablablablablablablablablablablablablablabla",
+               looking_for: "ps2,ps3,ps4, give me everything you have motherfucker!",
+               user_id: User.first.id,
+               category_id: Category.first.id
+               )
+end

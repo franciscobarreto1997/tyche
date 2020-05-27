5eff0a2ea2072c31ca37939abcb19494d04a948d
francisco <franciscoantunesbarreto1997@gmail.com>
2020-05-27 11:22:11 +0100

    index page looking sharp, making it responsive next

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,11 +16,16 @@
 
 Item.destroy_all
 
+looking_fors = ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+                "Lorem Ipsum is simply dummy text",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled"]
+
 
 12.times do
   Item.create!(title: "My item",
                description: "blablablablablablablablablablablablablablablablablablablabla",
-               looking_for: "ps2,ps3,ps4, give me everything you have motherfucker!",
+               looking_for: looking_fors.sample,
                user_id: User.first.id,
                category_id: Category.first.id
                )
